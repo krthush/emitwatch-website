49ee39dd07dfd89d06051ad307065bc751c3743e
krthush <33790334+krthush@users.noreply.github.com>
2018-02-27 23:37:27 +0000

    hotjar

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -88,6 +88,18 @@
         });
     </script>
 
+    <!-- Hotjar Tracking Code for https://ownemit.com/ -->
+    <script>
+        (function(h,o,t,j,a,r){
+            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+            h._hjSettings={hjid:796145,hjsv:6};
+            a=o.getElementsByTagName('head')[0];
+            r=o.createElement('script');r.async=1;
+            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+            a.appendChild(r);
+        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+    </script>
+
     <title>emit</title>
 
 </head>
