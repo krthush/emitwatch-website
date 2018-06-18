18e9dd968f467ccd04727cbf7e60d18f5cf1f0dc
krthush <33790334+krthush@users.noreply.github.com>
2018-06-18 21:54:12 +0100

    countdown for start

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -40,8 +40,12 @@
 
                 <div class="bottom-align">
                     <div class="textContainer"> 
-                        <div class="sublineText whiteText">Kickstarter Launch: </div>
-                        <div class="sublineText whiteText">99 Days </div>
+                        <div class="sublineText whiteText">
+                            Kickstarter Launch:
+                        </div>
+                        <div class="sublineText whiteText">
+                            <span id="kickstarterCountDown"></span>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -133,3 +137,33 @@
         </div>
     </div>
 </div>
+
+<script>
+// Set the date we're counting down to
+var countDownDate = new Date("July 31, 2018 23:59:59").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get todays date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now an the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="kickstarterCountDown"
+  document.getElementById("kickstarterCountDown").innerHTML = days + " Days";
+
+  // If the count down is finished, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("kickstarterCountDown").innerHTML = "EXPIRED";
+  }
+}, 1000);
+</script>
