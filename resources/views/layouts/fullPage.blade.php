63e831ce57cef829c599df6c744a02e67f44eda6
krthush <33790334+krthush@users.noreply.github.com>
2018-06-18 20:51:01 +0100

    activation of drag n drop?
    hopefully it works

--- resources/views/layouts/fullPage.blade.php
+++ resources/views/layouts/fullPage.blade.php
@@ -19,6 +19,7 @@
             navigationPosition: 'right',
             showActiveTooltip: true,
             dragAndMove: true,
+            fadingEffectKey: b3duZW1pdC5jb21fbGJwWkhKaFowRnVaRTF2ZG1VPTJ0cA==,
             sectionsColor: ['#0a0a0a', '#f5f5f5', '#0a0a0a', '#f5f5f5'],
         });
     });
