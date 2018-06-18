4aeabb2bb75f219423a54f8cfcf34082cdfe67f3
krthush <33790334+krthush@users.noreply.github.com>
2018-06-18 21:59:55 +0100

    spelling fixes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -60,7 +60,7 @@
                 <ul style="list-style-type:square">
                   <li>A different way of looking at time. In reverse.</li>
                   <li>Shows a countdown to start and end of day.</li>
-                  <li>Mutiple countdowns to most important events / goals/ look-forwards.</li>
+                  <li>Multiple countdowns to most important events/goals/look-forwards.</li>
                   <li>Customise countdowns to last from seconds to years.</li>
                   <li>Simply select an event on your phone calendar and emit syncs automatically.</li>
                   <li>Shows current time, if needed.</li>
