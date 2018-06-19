8416b4ac00dc88ac6a8addcd862eface0cf5eca6
krthush <33790334+krthush@users.noreply.github.com>
2018-06-19 11:06:36 +0100

    script fix on kickstarter countdown

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -29,6 +29,33 @@
 <script src="/js/fullpage.dragAndMove.min.js"></script>
 <script src="/js/jquery.fullpage.extensions.min.js"></script>
 
+<!-- Script for calculating days until kickstarterCountDown -->
+<script>
+$(function() {
+    var countDownDate = new Date("July 31, 2018 23:59:59").getTime();
+    // Get todays date and time
+    var now = new Date().getTime();
+
+    // Find the distance between now an the count down date
+    var distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Display the result in the element with id="kickstarterCountDown"
+    document.getElementById("kickstarterCountDown").innerHTML = days + " Days";
+
+    // If the count down is finished, write some text 
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("kickstarterCountDown").innerHTML = "EXPIRED";
+    }
+});
+</script>
+
 <!-- Hotjar Tracking Code for https://ownemit.com/ -->
 <script>
     (function(h,o,t,j,a,r){
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,36 +21,6 @@
     </div>
 @endif
 
-<script>
-// Set the date we're counting down to
-var countDownDate = new Date("July 31, 2018 23:59:59").getTime();
-
-// Update the count down every 1 second
-var x = setInterval(function() {
-
-  // Get todays date and time
-  var now = new Date().getTime();
-
-  // Find the distance between now an the count down date
-  var distance = countDownDate - now;
-
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-  // Display the result in the element with id="kickstarterCountDown"
-  document.getElementById("kickstarterCountDown").innerHTML = days + " Days";
-
-  // If the count down is finished, write some text 
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("kickstarterCountDown").innerHTML = "EXPIRED";
-  }
-}, 1000);
-</script>
-
 <div class="container-fluid zeroPad" id="fullpage">
 
     <div class="section">
