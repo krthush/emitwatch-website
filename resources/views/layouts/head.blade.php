c4efeb51433b2c8d26c8260f3b9e26ee0ffce293
krthush <33790334+krthush@users.noreply.github.com>
2018-06-22 12:20:50 +0100

    filename changes + upload

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -3,7 +3,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
 
-<meta property="og:image" content="https://ownemit.com/images/rightfacing_goal_comp.png" />
+<meta property="og:image" content="https://ownemit.com/images/rightfacing_goal_comp_bg.png" />
 
 <meta property="og:description" content="New perspective on time. In reverse." />
 
