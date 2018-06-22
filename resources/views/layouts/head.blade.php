477c56245716bb72618075777a9e2c3e5c35f4cb
krthush <33790334+krthush@users.noreply.github.com>
2018-06-22 13:07:38 +0100

    fixing meta data + laravel layout problem

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -2,6 +2,15 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+
+<meta property="og:image" content="https://ownemit.com/images/rightfacing_goal_comp_bg.png" />
+
+<meta property="og:description" content="New perspective on time. In reverse." />
+
+<meta property="og:url"content="https://ownemit.com/" />
+
+<meta property="og:title" content="emit" />
+
 <!-- CSS -->
 
 <!-- Custom Fonts -->
@@ -30,26 +39,6 @@
 <script src="/js/fullpage.dragAndMove.min.js"></script>
 <script src="/js/jquery.fullpage.extensions.min.js"></script>
 
-<!-- FullPage JS setup -->
-<script type="text/javascript">
-    $(document).ready(function() {
-        $('#fullpage').fullpage({
-            keyboardScrolling: true,
-            animateAnchor: true,
-            recordHistory: true,
-            navigation: true,
-            navigationPosition: 'right',
-            showActiveTooltip: true,
-            dragAndMove: false,
-            dragAndMoveKey: 'b3duZW1pdC5jb21fbGJwWkhKaFowRnVaRTF2ZG1VPTJ0cA==',
-            sectionsColor: ['#0a0a0a', '#f5f5f5', '#f5f5f5', '#f5f5f5'],
-            anchors: ['home', 'features', 'progress', 'mission'],
-            scrollOverflow: true,
-            verticalCentered:false,
-        });
-    });
-</script>
-
 <!-- Hotjar Tracking Code for https://ownemit.com/ -->
 <script>
     (function(h,o,t,j,a,r){
@@ -60,89 +49,6 @@
         r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
         a.appendChild(r);
     })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-</script>
-
-<!-- Script for calculating days until kickstarterCountDown -->
-<script>
-$(function() {
-    var countDownDate = new Date("July 31, 2018 23:59:59").getTime();
-    // Get todays date and time
-    var now = new Date().getTime();
-
-    // Find the distance between now an the count down date
-    var distance = countDownDate - now;
-
-    // Time calculations for days, hours, minutes and seconds
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-    // Display the result in the element with id="kickstarterCountDown"
-    document.getElementById("kickstarterCountDown").innerHTML = days + " Days";
-    document.getElementById("kickstarterCountDownMobile").innerHTML = days + " Days";
-
-    // If the count down is finished, write some text 
-    if (distance < 0) {
-        clearInterval(x);
-        document.getElementById("kickstarterCountDown").innerHTML = "BEGUN!";
-        document.getElementById("kickstarterCountDownMobile").innerHTML = "BEGUN!";
-    }
-});
-</script>
-
-<!-- JS for Daily CountDown -->
-<script type="text/javascript">
-
-    // Blank string
-    var string = "";
-    // Initial call
-    // Get date today, the subtract to get time to 24:00:00 (theoretical midnight)
-    var now = new Date();
-    var seconds = 59-now.getSeconds();
-    var minutes = 59-now.getMinutes();
-    var hours = 23-now.getHours();
-
-    // Makes sure time is in format of hh:mm:ss
-    if (hours   < 10) {hours   = "0"+hours;}
-    if (minutes < 10) {minutes = "0"+minutes;}
-    if (seconds < 10) {seconds = "0"+seconds;}
-
-    // Write time in hh:mm:ss format to element with #time
-    document.getElementById("time").innerHTML = string.concat(hours, ":", minutes, ":", seconds);
-    document.getElementById("time_small_screen").innerHTML = string.concat(hours, ":", minutes, ":", seconds);
-
-    // function to repeat every second
-    window.setInterval(function(){
-
-        var now = new Date();
-        var seconds = 59-now.getSeconds();
-        var minutes = 59-now.getMinutes();
-        var hours = 23-now.getHours();
-
-        // makes sure time is in format of hh:mm:ss
-        if (hours   < 10) {hours   = "0"+hours;}
-        if (minutes < 10) {minutes = "0"+minutes;}
-        if (seconds < 10) {seconds = "0"+seconds;}
-
-        // write time in hh:mm:ss format to element with #time
-        document.getElementById("time").innerHTML = string.concat(hours, ":", minutes, ":", seconds);
-        document.getElementById("time_small_screen").innerHTML = string.concat(hours, ":", minutes, ":", seconds);
-
-    }, 1000);
-
-    function msToTime(s) {
-
-        var ms = s % 1000;
-        s = (s - ms) / 1000;
-        var secs = s % 60;
-        s = (s - secs) / 60;
-        var mins = s % 60;
-        var hrs = (s - mins) / 60;
-
-        return hrs + ':' + mins + ':' + secs + '.' + ms;
-    }
-
 </script>
 
 <title>emit</title>
