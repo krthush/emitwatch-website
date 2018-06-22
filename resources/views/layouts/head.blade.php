96e62b5341903d2406bc058d5aeb7d730cabe5bd
krthush <33790334+krthush@users.noreply.github.com>
2018-06-22 13:22:33 +0100

    meta data change

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -4,8 +4,11 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
 <meta property="og:title" content="emit">
-<meta property="og:image" content="https://ownemit.com/images/rightfacing_goal_comp_bg.png">
+<meta property="og:image" content="https://ownemit.com/images/rightfacing_goal_comp_preview.png">
 <meta property="og:description" content="New perspective on time. In reverse.">
+<meta property="og:url" content="ownemit.com">
+<meta property="og:type" content="website">
+
 
 <!-- CSS -->
 
