80b06b22fcfb21b575951ec7fa3b5df52a76a2d8
krthush <33790334+krthush@users.noreply.github.com>
2018-07-13 12:16:06 +0100

    change date

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -122,7 +122,7 @@
                 <div class="col-lg-2"></div>
                 <div class="col-lg-8 generalFont mediumSize blackText">
 
-                    <p>We are currently working on our prototypes and will be fund-raising on Kickstarter (01/08/2018).</p>
+                    <p>We are currently working on our prototypes and will be fund-raising on Kickstarter (August 2018).</p>
 
                     <p>You can contact us at <a href="mailto:ownemit@gmail.com">ownemit@gmail.com</a> or +447472508476, and we'll get back to you as soon as possible.</p>
 
@@ -169,7 +169,7 @@
                 <div class="col-lg-2"></div>
                 <div class="col-lg-8 generalFont mediumSizeMobile blackText">
 
-                    <p>We are currently working on our prototypes and will be fund-raising on Kickstarter (01/08/2018).</p>
+                    <p>We are currently working on our prototypes and will be fund-raising on Kickstarter (August 2018).</p>
 
                     <p>You can contact us at <a href="mailto:ownemit@gmail.com">ownemit@gmail.com</a> or +447472508476, and we'll get back to you as soon as possible.</p>
 
