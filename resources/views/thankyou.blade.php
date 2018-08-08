5b123c25d2d3ac0342ad395a0d96517371a54255
krthush <33790334+krthush@users.noreply.github.com>
2018-08-08 18:04:25 +0100

    better thank you page

--- resources/views/thankyou.blade.php
+++ resources/views/thankyou.blade.php
@@ -1,97 +1,57 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.master')
 
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+@section('content')
+<!-- Content-->
 
+<div class="container-fluid zeroPad" id="fullpage">
 
-        <title>Thank you</title>
+    <div class="section">
+        <div class="d-none d-md-block mx-auto top-align full-width titleFont text-center titleSize blackText">THANK YOU</div>
+        <div class="d-md-none mx-auto top-align full-width titleFont text-center titleSizeMobile blackText">THANK YOU</div>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <div class="d-none d-md-block container-fluid">
+            <div class="row full-width sectionContainer mediumBottomPad">
+                <div class="col-sm-2"></div>
+                <div class="col-sm-8 generalFont generalSize blackText text-center">
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
+                    <b>Subscription confirmed.</b><br>
+                    <a class="licenseLink faded" href="\">Return to website</a>
+                     
                 </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Thank you for your subscription!
-                </div>
-
-                <div class="links">
-                    <a href="\">Return to website</a>
-                </div>
+                <div class="col-sm-2"></div>
             </div>
         </div>
-    </body>
-</html>
+        <div class="d-md-none container-fluid">
+            <div class="row full-width sectionContainerMobile largeBottomPad">
+                <div class="col-sm-2"></div>
+                <div class="col-sm-8 generalFont generalSize blackText text-center">
+
+                    <b>Subscription confirmed.</b><br>
+                    <a class="licenseLink faded" href="\">Return to website</a>
+
+                </div>
+                <div class="col-sm-2"></div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- FullPage JS setup -->
+<script type="text/javascript">
+    $(document).ready(function() {
+
+        $('#fullpage').fullpage({
+            keyboardScrolling: true,
+            animateAnchor: true,
+            recordHistory: true,
+            showActiveTooltip: true,
+            sectionsColor: ['#f5f5f5'],
+            anchors: ['mission'],
+            scrollOverflow: true,
+            verticalCentered:false,
+
+        });
+    });
+</script>
+
+@endsection
