f5c009a6f282e56e18d85521574166b23fd1d8db
krthush <33790334+krthush@users.noreply.github.com>
2018-10-09 14:19:39 +0100

    licenses update

--- resources/views/licenses.blade.php
+++ resources/views/licenses.blade.php
@@ -31,6 +31,7 @@
                     Heart by Gregor Cresnar from the Noun Project <br>
                     steps by Tobias Strohbach from the Noun Project <br>
                     <a class="licenseLink" href="https://www.youtube.com/watch?v=LHFXQ7Y2cos&index=5&list=PLESp7SFlcw7zHMdBttXsvWSQ1_Hi5Bt59&t=0s">Morning Walk</a> by Jonny Easton <br>
+                    Kickstarter campaign video produced with Giovanni Montalvo
                      
                 </div>
                 <div class="col-sm-2"></div>
