b2f95fb3dee07e75213e238976dc0c036face405
krthush <33790334+krthush@users.noreply.github.com>
2018-11-08 20:24:32 +0000

    Update resources/views/licenses.blade.php

--- resources/views/licenses.blade.php
+++ resources/views/licenses.blade.php
@@ -18,18 +18,63 @@
                 <div class="col-sm-8 generalFont generalSize blackText text-justify">
 
                     <b>Licenses:</b> <br>
+
                     day and night by Nerea Martínez Orduña from the Noun Project <br>
+
                     Smartphone by Naveen from the Noun Project <br>
+
                     timer by Adrien Coquet from the Noun Project <br>
+
                     Watch by Manasa from the Noun Project <br>
+
                     Calendar by Storm Icons from the Noun Project <br>
+
                     Battery by Gregor Cresnar from the Noun Project <br>
+
                     timer by Anusha Narvekar from the Noun Project <br>
+
                     Time by arjuazka from the Noun Project <br>
-                    shopping by Adrien Coquet from the Noun Project <br>
+
+                    shopping by Alfa Design from the Noun Project <br>
+
                     Home by gatot supriyadi from the Noun Project <br>
+
                     Heart by Gregor Cresnar from the Noun Project <br>
+
                     steps by Tobias Strohbach from the Noun Project <br>
+
+                    User by Atif Arshad from the Noun Project <br>
+
+                    testing by Dinosoft Labs from the Noun Project <br>
+
+                    shipping by asianson.design from the Noun Project <br>
+
+                    manufacturing by Made from the Noun Project <br>
+
+                    Electronics by Maxim Basinski from the Noun Project <br>
+
+                    cans by Veremeya from the Noun Project <br>
+
+                    Blueprint by Nikita Kozin from the Noun Project <br>
+
+                    Idea by Adrien Coquet from the Noun Project <br>
+
+                    delivery by joeartcon from the Noun Project <br>
+
+                    tools by by Kenton Quatman from the Noun Project <br>
+
+                    test by ChangHoon Baek from the Noun Project <br>
+
+                    production by AdbA Icons ❤️ from the Noun Project <br>
+
+                    people by Mello from the Noun Project <br>
+
+                    User Interface by Susannanova from the Noun Project <br>
+
+                    Bluetooth by Viktor Vorobyev from the Noun Project <br>
+
+                    partnership by Artem  Kovyazin from the Noun Project <br>
+
                     <a class="licenseLink" href="https://www.youtube.com/watch?v=LHFXQ7Y2cos&index=5&list=PLESp7SFlcw7zHMdBttXsvWSQ1_Hi5Bt59&t=0s">Morning Walk</a> by Jonny Easton <br>
                     Kickstarter campaign video produced with Giovanni Montalvo
                      
