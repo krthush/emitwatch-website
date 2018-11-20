75062da84e14dd5aae04722d9e24ef7039b6fd03
krthush <33790334+krthush@users.noreply.github.com>
2018-11-20 10:23:30 +0000

    Update resources/views/presskit.blade.php

--- resources/views/presskit.blade.php
+++ resources/views/presskit.blade.php
@@ -12,7 +12,7 @@
         <div class="container-fluid miniTopPad">
             <div class="row full-width sectionContainer mediumBottomPad">
                 <div class="col-sm-2"></div>
-                <div class="col-sm-4 generalFont blackText text-center">
+                <div class="col-sm-2 generalFont blackText text-center">
                     <div class="mediumSize">
                         <b>Media Kit</b><br>                        
                     </div>
@@ -20,12 +20,20 @@
                         <a class="licenseLink faded" href="https://1drv.ms/b/s!AgYYSjzpoQzmihvsnncAhx6oIfep" target="_blank">Download - OneDrive</a>    
                     </div>                     
                 </div>
-                <div class="col-sm-4 generalFont blackText text-center">
+                <div class="col-sm-2 generalFont blackText text-center">
                     <div class="mediumSize">
                         <b>High-Res Images</b><br>                        
                     </div>
                     <div class="generalSize">
                         <a class="licenseLink faded" href="https://1drv.ms/f/s!AgYYSjzpoQzmmRL07tWHCg5MSDel" target="_blank">Download - OneDrive</a>    
+                    </div>                     
+                </div>
+                <div class="col-sm-2 generalFont blackText text-center">
+                    <div class="mediumSize">
+                        <b>Pitch Deck</b><br>                        
+                    </div>
+                    <div class="generalSize">
+                        <a class="licenseLink faded" href="https://1drv.ms/b/s!AgYYSjzpoQzmo2aoxesJG2mHeJw7" target="_blank">Download - OneDrive</a>    
                     </div>                     
                 </div>
                 <div class="col-sm-2"></div>
