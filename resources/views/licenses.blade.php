c304ad9a43a972b66406e28902a17f38588ae93b
krthush <33790334+krthush@users.noreply.github.com>
2019-01-08 18:38:03 +0000

    Update licenses.blade.php

--- resources/views/licenses.blade.php
+++ resources/views/licenses.blade.php
@@ -83,6 +83,14 @@
 
                     global by Chameleon Design from the Noun Project <br>
 
+                    Social Media by Mat fine from the Noun Project <br>
+
+                    Celebrity Advertising by Creative Stall from the Noun Project <br>
+
+                    Corporation by Nithinan Tatah from the Noun Project <br>
+
+                    analytics by Shastry from the Noun Project <br>
+
                     <a class="licenseLink" href="https://www.youtube.com/watch?v=LHFXQ7Y2cos&index=5&list=PLESp7SFlcw7zHMdBttXsvWSQ1_Hi5Bt59&t=0s">Morning Walk</a> by Jonny Easton <br>
                     Kickstarter campaign video produced with Giovanni Montalvo
                      
