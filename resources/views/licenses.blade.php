d8b776a42145a0ec63985af692a1dd0b4fdd9010
krthush <33790334+krthush@users.noreply.github.com>
2019-01-08 14:06:28 +0000

    Update licenses.blade.php

--- resources/views/licenses.blade.php
+++ resources/views/licenses.blade.php
@@ -77,6 +77,12 @@
 
                     Scalability by Stefan Traistaru from the Noun Project <br>
 
+                    b2b by Becris from the Noun Project <br>
+
+                    AI by mungang kim from the Noun Project <br>
+
+                    global by Chameleon Design from the Noun Project <br>
+
                     <a class="licenseLink" href="https://www.youtube.com/watch?v=LHFXQ7Y2cos&index=5&list=PLESp7SFlcw7zHMdBttXsvWSQ1_Hi5Bt59&t=0s">Morning Walk</a> by Jonny Easton <br>
                     Kickstarter campaign video produced with Giovanni Montalvo
                      
