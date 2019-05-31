d6f9b60e0d5f355e302f5caef8f35f225ba32b1c
krthush <33790334+krthush@users.noreply.github.com>
2019-05-31 18:41:07 +0100

    Update watch-android-terms.blade.php

--- resources/views/watch-android-terms.blade.php
+++ resources/views/watch-android-terms.blade.php
@@ -126,8 +126,7 @@
                 <p>These Terms of Use and any policies or operating rules posted by us on the App constitute the entire agreement and understanding between you and us. Our failure to exercise or enforce any right or provision of these Terms of Use shall not operate as a waiver of such right or provision. These Terms of Use operate to the fullest extent permissible by law. We may assign any or all of our rights and obligations to others at any time. We shall not be responsible or liable for any loss, damage, delay, or failure to act caused by any cause beyond our reasonable control. If any provision or part of a provision of these Terms of Use is determined to be unlawful, void, or unenforceable, that provision or part of the provision is deemed severable from these Terms of Use and does not affect the validity and enforceability of any remaining provisions. There is no joint venture, partnership, employment or agency relationship created between you and us as a result of these Terms of Use or use of the App. You agree that these Terms of Use will not be construed against us by virtue of having drafted them. You hereby waive any and all defenses you may have based on the electronic form of these Terms of Use and the lack of signing by the parties hereto to execute these Terms of Use.</p>
                 <h1>CONTACT US&nbsp;</h1>
                 <p>In order to resolve a complaint regarding the App or to receive further information regarding use of the App, please contact us at team@ownemit.com</p>
-                <p>Rubik's Cubes make a perfect gift for any occasion. Learn more about this amazing puzzle on <a href="https://ruwix.com/" rel="nofollow noopener">this link</a>.</p>
-                     
+                
                 </div>
                 <div class="col-sm-2"></div>
             </div>
